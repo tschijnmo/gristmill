ab3433de976f74a1095d29192f014e3e5836cba2
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2017-03-13 22:57:34 -0500

    Forward the base printer to the base package

--- gristmill/__init__.py
+++ gristmill/__init__.py
@@ -3,6 +3,9 @@
 Public names are going to be imported here.
 """
 
+from .generate import (
+    BasePrinter
+)
 from .optimize import optimize, verify_eval_seq
 from .utils import get_flop_cost
 
@@ -10,4 +13,5 @@ __all__ = [
     'optimize',
     'verify_eval_seq',
     'get_flop_cost',
+    'BasePrinter',
 ]
