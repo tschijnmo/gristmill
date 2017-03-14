3793b9d8d356487e495046cedb6e6c15b5923e2b
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2017-03-13 23:08:47 -0500

    Add tests for the base printer
    The test case is made to use a very colour input tensor for better code
    coverage.

---
+++ tests/printers_test.py
@@ -0,0 +1,126 @@
+"""Tests for the base printer.
+"""
+
+import pytest
+from drudge import Drudge, Range
+from sympy import Symbol, IndexedBase, symbols, sin
+from sympy.printing.python import PythonPrinter
+
+from gristmill import BasePrinter
+
+
+@pytest.fixture(scope='module')
+def simple_drudge(spark_ctx):
+    """Form a simple drudge with some basic information.
+    """
+
+    dr = Drudge(spark_ctx)
+
+    n = Symbol('n')
+    r = Range('R', 0, n)
+
+    dumms = symbols('a b c d e f g')
+    dr.set_dumms(r, dumms)
+    dr.add_resolver_for_dumms()
+
+    return dr
+
+
+@pytest.fixture
+def colourful_tensor(simple_drudge):
+    """Form a colourful tensor definition capable of large code coverage.
+    """
+
+    dr = simple_drudge
+    p = dr.names
+
+    x = IndexedBase('x')
+    u = IndexedBase('u')
+    v = IndexedBase('v')
+    dr.set_name(x, u, v)
+
+    r, s = symbols('r s')
+    dr.set_name(r, s)
+
+    a, b, c = p.R_dumms[:3]
+
+    tensor = dr.define(x[a, b], (
+        ((2 * r) / (3 * s)) * u[b, a] -
+        dr.sum((c, p.R), u[a, c] * v[c, b] * sin(c) / 2)
+    ))
+
+    return tensor
+
+
+def test_base_printer_ctx(simple_drudge, colourful_tensor):
+    """Test the context formation facility in base printer."""
+
+    dr = simple_drudge
+    p = dr.names
+    tensor = colourful_tensor
+
+    def proc_indexed(ctx):
+        """Process indexed names by mangling the base name."""
+        ctx.base = ctx.base + str(len(ctx.indices))
+        return
+
+    printer = BasePrinter(PythonPrinter(), proc_indexed)
+    ctx = printer.transl(tensor)
+
+    def check_range(ctx, index):
+        """Check the range information in a context for a index."""
+        assert ctx.index == index
+        assert ctx.range == p.R
+        assert ctx.lower == '0'
+        assert ctx.upper == 'n'
+        assert ctx.size == 'n'
+
+    assert ctx.base == 'x2'
+    for i, j in zip(ctx.indices, ['a', 'b']):
+        check_range(i, j)
+        continue
+
+    assert len(ctx.terms) == 2
+    for term in ctx.terms:
+        if len(term.sums) == 0:
+            # The transpose term.
+
+            assert term.phase == '+'
+            assert term.numerator == '2*r'
+            assert term.denominator == '(3*s)'
+
+            assert len(term.indexed_factors) == 1
+            factor = term.indexed_factors[0]
+            assert factor.base == 'u2'
+            for i, j in zip(factor.indices, ['b', 'a']):
+                check_range(i, j)
+                continue
+
+            assert len(term.other_factors) == 0
+
+        elif len(term.sums) == 1:
+
+            check_range(term.sums[0], 'c')
+
+            assert term.phase == '-'
+            assert term.numerator == '1'
+            assert term.denominator == '2'
+
+            assert len(term.indexed_factors) == 2
+            for factor in term.indexed_factors:
+                if factor.base == 'u2':
+                    expected = ['a', 'c']
+                elif factor.base == 'v2':
+                    expected = ['c', 'b']
+                else:
+                    assert False
+                for i, j in zip(factor.indices, expected):
+                    check_range(i, j)
+                    continue
+                continue
+
+            assert len(term.other_factors) == 1
+            assert term.other_factors[0] == 'sin(c)'
+
+        else:
+            assert False
