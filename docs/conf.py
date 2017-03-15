5a989ed7edc34a4785cb7cabd4ee51f007cff9f7
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2017-03-14 22:39:47 -0500

    Update documentation configuration
    1. Napoleon extension is loaded.
    
    2. The member definition order is preserved.

--- docs/conf.py
+++ docs/conf.py
@@ -31,10 +31,13 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
+extensions = [
+    'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode']
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -156,5 +159,5 @@ texinfo_documents = [
      'Miscellaneous'),
 ]
 
-
+autodoc_member_order = 'bysource'
 
