c4da9459a1beb2216800e622750573c34fd8b5ba
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2017-03-20 23:44:41 -0500

    Bump version to 0.3.0dev

--- gristmill/__init__.py
+++ gristmill/__init__.py
@@ -10,7 +10,7 @@ from .generate import (
 from .optimize import optimize, verify_eval_seq
 from .utils import get_flop_cost
 
-__version__ = '0.2.0'
+__version__ = '0.3.0dev'
 
 __all__ = [
     'optimize',
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ CLASSIFIERS = [
 
 setup(
     name='gristmill',
-    version='0.2.0',
+    version='0.3.0dev',
     description=DESCRIPTION.splitlines()[0],
     long_description=DESCRIPTION,
     url='https://github.com/tschijnmo/gristmill',
