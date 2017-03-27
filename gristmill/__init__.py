fa9fdf718f0bdbb3e56bfc74edb8cff119118c7b
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2017-03-27 17:37:09 -0500

    Forward strategy enumeration to base package

--- gristmill/__init__.py
+++ gristmill/__init__.py
@@ -7,12 +7,13 @@ from .generate import (
     BasePrinter, ImperativeCodePrinter, CCodePrinter, FortranPrinter,
     EinsumPrinter
 )
-from .optimize import optimize, verify_eval_seq
+from .optimize import optimize, verify_eval_seq, Strategy
 from .utils import get_flop_cost
 
 __version__ = '0.3.0dev'
 
 __all__ = [
+    'Strategy',
     'optimize',
     'verify_eval_seq',
     'get_flop_cost',
