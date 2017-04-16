85d74a289237e27c71602422484871b1a382dd01
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2017-04-15 23:08:18 -0500

    Add tests on optimization with complex conjugate

--- tests/misc_test.py
+++ tests/misc_test.py
@@ -1,7 +1,7 @@
 """Test optimization of different special kinds of tensors."""
 
-from drudge import Drudge
-from sympy import symbols
+from drudge import Drudge, Range
+from sympy import symbols, IndexedBase, conjugate
 
 from gristmill import optimize, verify_eval_seq
 
@@ -17,5 +17,29 @@ def test_simple_scalar_optimization(spark_ctx):
 
     a, b, r = symbols('a b r')
     targets = [dr.define(r, a * b)]
+    eval_seq = optimize(targets)
+    assert verify_eval_seq(eval_seq, targets)
+
+
+def test_conjugation_optimization(spark_ctx):
+    """Test optimization of expressions containing complex conjugate.
+    """
+
+    dr = Drudge(spark_ctx)
+
+    n = symbols('n')
+    r = Range('r', 0, n)
+    a, b, c, d = symbols('a b c d')
+    dr.set_dumms(r, [a, b, c, d])
+    dr.add_default_resolver(r)
+
+    p = IndexedBase('p')
+    x = IndexedBase('x')
+    y = IndexedBase('y')
+    z = IndexedBase('z')
+
+    targets = [dr.define_einst(
+        p[a, b], x[a, c] * conjugate(y[c, b]) + x[a, c] * conjugate(z[c, b])
+    )]
     eval_seq = optimize(targets)
     assert verify_eval_seq(eval_seq, targets)
