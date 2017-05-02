14b4a443e528430e99125912f9b20a3ea96f706b
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2017-05-02 02:14:00 -0500

    Revert back to unity for delta of empty bicliques
    It is realized that the excess cost for any node for an empty biclique
    will be zero.  So there is no need to use infinity to mask the excess
    cost.

--- gristmill/optimize.py
+++ gristmill/optimize.py
@@ -10,7 +10,7 @@ import warnings
 from drudge import TensorDef, prod_, Term, Range, sum_
 from sympy import (
     Integer, Symbol, Expr, IndexedBase, Mul, Indexed, sympify, primitive, Wild,
-    default_sort_key, oo
+    default_sort_key
 )
 from sympy.utilities.iterables import multiset_partitions
 
@@ -328,8 +328,9 @@ def _get_collect_saving(coeffs: _CostCoeffs, n_s: typing.Sequence[int]):
 
     if all(i == 0 for i in n_s):
         # This could allow bicliques empty in both directions to be augmented by
-        # any left or right term.
-        deltas = (oo, oo)
+        # any left or right term.  A value of unity is sufficient in that the
+        # excess cost computed based on the empty biclique is always zero.
+        deltas = (_UNITY, _UNITY)
     else:
         deltas = tuple(
             i * coeffs.final - j
