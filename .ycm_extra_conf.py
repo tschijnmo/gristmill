eb3bb70051e1a36b4968c56cbd042796a1a6c926
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2017-12-21 00:03:49 -0600

    Update YCM config
    Now the include directories for all the internal dependencies as well as
    the system Python3 includes are automatically set for YCM.

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -1,3 +1,19 @@
+import os.path
+import subprocess
+
 def FlagsForFile(filename, **kwargs):
     flags = ['-std=c++14', '-I/usr/local/include']
+
+    proj_dir = os.path.dirname(os.path.realpath(__file__))
+    for i in ['cpypp', 'fbitset', 'libparenth']:
+        flags.append('-I' + os.path.join(proj_dir, 'deps', i, 'include'))
+        continue
+
+    # Call the config script to get the directories for the Python3 interpreter
+    # in PATH.
+    py_includes = subprocess.check_output(
+        ['python3-config', '--includes']
+    ).decode('utf-8').split()
+    flags.extend(py_includes)
+
     return {'flags': flags}
